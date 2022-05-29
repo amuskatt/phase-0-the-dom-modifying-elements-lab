c60b0389cafb0b8d81200efb1d801bd08657dfdc
amuskatt <102235034+amuskatt@users.noreply.github.com>
2022-05-29 10:44:07 -0400

    commit tree 07fdc2a851ec7131472e2e06e88af175bb62d594

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement(`h1`);
+newHeader.id = "victory";
+newHeader.textContent = "Adam is the champion" 
+document.body.append(newHeader)
+
+
+
+
+
+
+
+
+
